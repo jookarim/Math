bb1dd6f0a1f92fca6cdb7410aaade8db4947b9bf
Youssef karim <joo.karim2013@gmail.com>
2026-03-07 15:39:19 +0200

    Add vector3 operators

--- include/Vector3.h
+++ include/Vector3.h
@@ -1,18 +1,22 @@
 #include <cmath>
 #include <ostream>
+#include <stdexcept>
 
 namespace Math
 {
     template<typename T>
     class Vector3
     {
+    private:
+        static constexpr const char* VECTOR3_ERROR = "Error: Vector3 divide by 0";
+
     public:
         T x, y, z;
 
         Vector3() = default;
         ~Vector3() = default;
 
-        void setVectorData(const T& x, const T& y, const T& z)
+        void setVectorData(const T& x, const T& y, const T& z) noexcept
         {
             this->x = x;
             this->y = y;
@@ -24,12 +28,7 @@ namespace Math
             setVectorData(x, y, z);
         }
 
-        Vector3(const Vector3& other) noexcept
-        {
-            x = other.x;
-            y = other.y;
-            z = other.z;
-        }
+        Vector3(const Vector3& other) = default;
 
         Vector3& operator=(const Vector3& other) noexcept
         {
@@ -42,48 +41,190 @@ namespace Math
             return *this;
         }
 
-        Vector3(Vector3&& other) noexcept
+        Vector3 operator*(const Vector3& other) const noexcept
         {
-            x = other.x;
-            y = other.y;
-            z = other.z;
+            return {
+                x * other.x,
+                y * other.y,
+                z * other.z
+            };
         }
 
-        Vector3& operator=(Vector3&& other) noexcept
+        Vector3 operator/(const Vector3& other) const
         {
-            if (this == &other)
+            if (other.x != 0 && other.y != 0 && other.z != 0)
             {
-                return *this;
+                return {
+                    x / other.x,
+                    y / other.y,
+                    z / other.z
+                };
             }
 
-            x = other.x;
-            y = other.y;
-            z = other.z;
-
-            other.x = 0;
-            other.y = 0;
-            other.z = 0;
-
-            return *this;
+            throw std::runtime_error(VECTOR3_ERROR);
         }
 
-        float length() const
+        Vector3 operator*(const T& scaler) const noexcept
+        {
+            return {
+                x * scaler,
+                y * scaler,
+                z * scaler
+            };
+        }
+
+        Vector3 operator/(const T& scaler) const
+        {
+            if (scaler != 0)
+            {
+                return {
+                    x / scaler,
+                    y / scaler,
+                    z / scaler
+                };
+            }
+
+            throw std::runtime_error(VECTOR3_ERROR);
+        }
+
+        void operator/=(const Vector3& other)
+        {
+            if (other.x != 0 && other.y != 0 && other.z != 0)
+            {
+                x /= other.x;
+                y /= other.y;
+                z /= other.z;
+                return;
+            }
+
+            throw std::runtime_error(VECTOR3_ERROR);
+        }
+
+        void operator*=(const Vector3& other) noexcept
+        {
+            x *= other.x;
+            y *= other.y;
+            z *= other.z;
+        }
+
+        void operator*=(const T& scaler) noexcept
+        {
+            x *= scaler;
+            y *= scaler;
+            z *= scaler;
+        }
+
+        void operator/=(const T& scaler)
+        {
+            if (scaler != 0)
+            {
+                x /= scaler;
+                y /= scaler;
+                z /= scaler;
+                return;
+            }
+
+            throw std::runtime_error(VECTOR3_ERROR);
+        }
+
+        Vector3 operator+(const Vector3& other) const noexcept
+        {
+            return {
+                x + other.x,
+                y + other.y,
+                z + other.z
+            };
+        }
+
+        Vector3 operator-(const Vector3& other) const noexcept
+        {
+            return
+            {
+                x - other.x,
+                y - other.y,
+                z - other.z
+            };
+        }
+
+        void operator+=(const Vector3& other) noexcept
+        {
+            x += other.x;
+            y += other.y;
+            z += other.z;
+        }
+
+        void operator-=(const Vector3& other) noexcept
+        {
+            x -= other.x;
+            y -= other.y;
+            z -= other.z;
+        }
+
+        Vector3 operator+(const T& scaler) const noexcept
+        {
+            return {
+                x + scaler,
+                y + scaler,
+                z + scaler
+            };
+        }
+
+        Vector3 operator-(const T& scaler) const noexcept
+        {
+            return {
+                x - scaler,
+                y - scaler,
+                z - scaler
+            };
+        }
+
+        void operator+=(const T& scaler) noexcept
+        {
+            x += scaler;
+            y += scaler;
+            z += scaler;
+        }
+
+        void operator-=(const T& scaler) noexcept
+        {
+            x -= scaler;
+            y -= scaler;
+            z -= scaler;
+        }
+
+        T length() const
         {
             return std::sqrt(x * x + y * y + z * z);
         }
 
         void normalize()
         {
-            float len = length();
+            T len = length();
 
-            x /= len;
-            y /= len;
-            z /= len;
+            if (len != 0)
+            {
+                x /= len;
+                y /= len;
+                z /= len;
+                return;
+            }
+
+            throw std::runtime_error(VECTOR3_ERROR);
+        }
+
+        
+        friend Vector3 operator*(const T& value, const Vector3& vec) noexcept
+        {
+            return {
+                vec.x * value,
+                vec.y * value,
+                vec.z * value
+            };
         }
     };
 
     template<typename T>
-    std::ostream& operator<<(std::ostream& o, const Vector3<T>& v)
+    std::ostream& operator<<(std::ostream& o, const Vector3<T>& v) noexcept
     {
         o << v.x << " " << v.y << " " << v.z;
         return o;
--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,25 @@ int main()
 {
 	Math::Vector3<float> v;
 	
-	v.setVectorData(1.f, 2.f, 3.f);
-	std::cout << v.length() << "\n";
-
-	v.normalize();
-	std::cout << v.length() << "\n";
-
-	std::cout << v << "\n";
+	v.setVectorData(1.f, 2.f, 0.f);
 
 	Math::Vector3<float> v2(v);
 
 	std::cout << v2 << "\n";
+
+	Math::Vector3<float> devision = 2 * v;
+
+	try
+	{
+		devision /= v;
+	}
+	catch (std::runtime_error& r)
+	{
+		std::cout << r.what() << "\n";
+	}
+	
+
+	std::cout << devision;
 
 	return 0;
 }
